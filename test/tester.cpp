4a5974a36e2288b4a18c2b0ee393cb716472632e
Edman P. Anjos <edmanjos@gmail.com>
2015-06-02 20:58:14 +0900

    Update tester.cpp to generate performance measurements

--- test/tester.cpp
+++ test/tester.cpp
@@ -2,6 +2,7 @@
 #include "gtest/gtest.h"
 #include <cmath>
 #include <cstdio>
+#include <chrono>
 #include <iostream>
 #include "rapidjson/document.h"
 #include "rapidjson/writer.h"
@@ -12,10 +13,25 @@
 #include "../src/serialize.hpp"
 
 using namespace std;
+using namespace std::chrono;
 using namespace rapidjson;
+
+high_resolution_clock::time_point t1, t2, bt1, bt2;
+bool dot1 = false, dobt1 = false;
+
+long long tick(string msg="time", bool bt=false);
+Document wow2(int k = 1);
+void print_parser2(Parser &p);
+
+char fn[100];
 
 Document wow(int k = 1);
 void print_parser(Parser &p);
+void filename(int k, char *fin=fn);
+void filenamec(int k, char *fin=fn);
+void filenameh(int k, char *fin=fn);
+void log_parser_size(Parser&, long long, int);
+long filesize(int k = 1);
 
 TEST(RapidJsonTest, ReadingStuff) {
     // 1. Parse a JSON string into DOM.
@@ -74,7 +90,7 @@ TEST(ParserTest, Dblp) {
     // print_parser(p);
 }
 
-TEST(Serialization, get_size) {
+TEST(SerializationTest, get_size) {
     Document d = wow(2);
     Parser p(d);
     int size = get_size(p);
@@ -83,7 +99,7 @@ TEST(Serialization, get_size) {
     ASSERT_EQ(p.tree.size_in_bytes(), 3);
 }
 
-TEST(Serialization, SuccinctTree) {
+TEST(SerializationTest, SuccinctTree) {
     Document d = wow(2);
     int size = 8;
     SuccinctTree s(d, size);
@@ -100,7 +116,7 @@ TEST(Serialization, SuccinctTree) {
     // cout << sa<< endl;;
 }
 
-TEST(Serialization, IntAndString) {
+TEST(SerializationTest, IntAndString) {
     int v = 10;
     StreamType res;
     serialize(v, res);
@@ -133,15 +149,18 @@ TEST(Serialization, IntAndString) {
     ASSERT_EQ(s, t);
 }
 
-TEST(Serialization, SaveLoadFile) {
-    // string fn = "test/tfile.bin";
-    for (int i = 1; i <= 3; ++i) {
+TEST(SerializationTest, ParserSize) {
+    for (int i = 5; i <= 9; ++i) {
+            // cout << "start" << endl;
         Document d = wow(i);
+            // cout << "loaded with rapidjson" << endl;
+        tick();
         Parser p(d);
-
-        save_to_file(p, "test/tfile.bin");
-        Parser pa = load_from_file("test/tfile.bin");
-        ASSERT_EQ(p, pa);
+        long long dur = tick();
+            // cout << "constructed parser" << endl;
+        filenamec(i);
+        save_to_file(p, string(fn));
+        log_parser_size(p, dur, i);
     }
 }
 
@@ -159,12 +178,39 @@ void print_parser(Parser &p) {
         cout << p.codes[i] << "|"; cout << endl;
 }
 
+long filesize(int k) {
+    char fn[100];
+    filename(k, fn);
+    ifstream ifile;
+    ifile.open(fn, ios::binary);
+    ifile.seekg(0, ifile.end);
+    long size = ifile.tellg();
+    ifile.close();
+    return size;
+}
+
+void filename(int k, char *fn) {
+    sprintf(fn, "test/sample%d.json", k);
+    if (k == 10) sprintf(fn, "test/dblp_.json");
+    if (k == 11) sprintf(fn, "test/dblp.json");
+}
+
+void filenamec(int k, char *fn) {
+    filename(k, fn);
+    sprintf(fn, "%s_c", fn);
+}
+
+void filenameh(int k, char *fn) {
+    filename(k, fn);
+    sprintf(fn, "%s_h", fn);
+}
+
 Document wow(int k) {
     Document d;
     char fn[100];
-    sprintf(fn, "../test/sample%d.json", k);
+    filename(k, fn);
     FILE *fp = fopen(fn, "r");
-    if (!fp) sprintf(fn, "test/sample%d.json", k), fp = fopen(fn, "r");
+    if (!fp) sprintf(fn, "../%s", fn), fp = fopen(fn, "r");
     if (!fp) {
         cout << "line: " << fn << endl;
         cout << "Sample file not found\n";
@@ -191,3 +237,78 @@ Document wow(int k) {
 
     return d;
 }
+
+void write_formatted(ofstream &f, const string &msg, long &a, size_t &b) {
+    f << msg << "\t\t(" << (int)((double)a/b*100) << "%)\t\t" << a << endl;
+}
+
+void log_parser_size(Parser &obj, long long dur, int k) {
+    filenameh(k);
+    ofstream mfile(fn, ios::out);
+    // original file size
+    long orig = filesize(k);
+    mfile << fn << endl;
+    mfile << "original size (bytes): " << orig << endl;
+
+    long header, tree, codes, names, values;
+    // size of header
+    size_t s = 0, b = 0;
+    s += 3 * sizeof(int);
+    header = s-b;
+    // size of tree
+    s += get_size(obj.tree);
+    tree = s-b; b = s;
+    // size of encodes, names, and values
+    for (int i = 0; i < obj.size; ++i)
+        s += get_size(obj.codes[i]);
+    codes = s-b; b = s;
+    for (int i = 0; i < obj.namen; ++i)
+        s += get_size(obj.names[i]);
+    names = s-b; b = s;
+    // mfile << ".." << i << ": "<< get_size(obj.values[i]) << endl;;}
+    for (int i = 0; i < obj.valuen; ++i)
+        s += get_size(obj.values[i]);
+    values = s-b;
+
+
+    write_formatted(mfile, ".header", header, s);
+    write_formatted(mfile, ".tree", tree, s);
+    write_formatted(mfile, ".codes", codes, s);
+    write_formatted(mfile, ".names", names, s);
+    write_formatted(mfile, ".values", values, s);
+    mfile << "Total " << s << endl;
+
+    // compression ratio
+    mfile << "Compression ratio (bigger is better): " << (double)orig/s << endl;
+    mfile << "Compression time: " << dur << " millis" << endl;
+    mfile << endl;
+
+    cout << fn << endl;
+    cout << "original size: " << orig << endl;
+    cout << "compressed: " << s << endl;
+    cout << "ratio (bigger better): " << (double)orig/s << endl << endl;
+    // cout << "time: " << dur << "ms" << endl << endl;
+
+    mfile.close();
+}
+
+long long tick(string msg, bool bt) {
+    high_resolution_clock::time_point *ts, *te;
+    bool *do1;
+    bt ? (ts = &bt1, te = &bt2, do1 = &dobt1) : (ts = &t1, te = &t2, do1 = &dot1);
+    *do1 ^= 1;
+    if (*do1) {
+        cout << "Timer start..." << endl;
+        *ts = high_resolution_clock::now();
+        return duration_cast<milliseconds>(*te - *ts).count();
+    }
+    else {
+        *te = high_resolution_clock::now();
+        // auto tot_time = duration_cast<microseconds>(*te - *ts).count();
+        auto tot_time = duration_cast<milliseconds>(*te - *ts).count();
+        cout << msg << ": " << tot_time << " millis" << endl;
+        return tot_time;
+    }
+}
+
+
