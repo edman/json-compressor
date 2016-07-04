463a029d29f1d15a83003a037ee2df52591e3202
Edman P. Anjos <edmanjos@gmail.com>
2016-07-04 21:52:12 +0900

    implements stringValue and jval in cjson

--- src/cjson.cpp
+++ src/cjson.cpp
@@ -5,7 +5,7 @@
 #include "cjson.hpp"
 #include "bp_tree.hpp"
 #include "df_tree.hpp"
-#include "jvalue.hpp"
+#include "jval.hpp"
 #include "util.hpp"
 #include "rapidjson/document.h"
 #include <iostream>
@@ -16,48 +16,41 @@ using namespace std;
 using namespace rapidjson;
 
 template <class SuccinctTree>
-Cjson<SuccinctTree>::Cjson(Value &d, bool debug) {
-    if (debug) cout << ".. start new cjson" << endl;
-
-    // Load names and values arrays
+Cjson<SuccinctTree>::Cjson(Value &d) {
+    // Load names table and values packed array
     unordered_map<string, int> nameTable;
     loadInfo(d, nameTable);
-    // Load the values bitmap
+    // Load values bitmap index
     values.loadBitmap();
 
     size = values.size();
-    if (debug) cout << ".. load info" << endl;
-    if (debug) cout << "names size " << names.size() << " values size " << values.size() << endl;
 
     // Reduce excess vector capacity
     names.shrink_to_fit();
+    stringValues.shrink_to_fit();
 
     // Construct a tree from the JSON sctructure
     tree = SuccinctTree(d, size);
-    if (debug) cout << ".. construct tree" << endl;
 }
 
 template <class SuccinctTree>
-void Cjson<SuccinctTree>::loadInfo(Value &d, unordered_map<string, int> &nameTable) {
-    if (d.IsObject()) {
+void Cjson<SuccinctTree>::loadInfo(Value &d, unordered_map<string,
+        int> &nameTable, uint nameId) {
+    resolveValue(d, nameId);
+    if (d.IsObject())
         for (auto it = d.MemberBegin(); it != d.MemberEnd(); ++it) {
             uint nameId = resolveNameId(it->name.GetString(), nameTable);
-            values.append<Jvalue>(Jvalue::factory(it->value, nameId));
-
-            loadInfo(it->value, nameTable);
+            loadInfo(it->value, nameTable, nameId);
         }
-    }
     else if (d.IsArray())
         for (auto it = d.Begin(); it != d.End(); ++it) {
-            uint nameId = resolveNameId("", nameTable);
-            values.append<Jvalue>(Jvalue::factory(*it, nameId));
-
             loadInfo(*it, nameTable);
         }
 }
 
 template <class SuccinctTree>
-uint Cjson<SuccinctTree>::resolveNameId(const string &name, unordered_map<string, int> &nameTable) {
+uint Cjson<SuccinctTree>::resolveNameId(const string &name,
+        unordered_map<string, int> &nameTable) {
     auto it = nameTable.find(name);
     if (it == nameTable.end()) {
         uint nameId = names.size();
@@ -68,28 +61,83 @@ uint Cjson<SuccinctTree>::resolveNameId(const string &name, unordered_map<string
     return it->second;
 }
 
-// int resolveName(const string &name, map<string, int> &nameMap, int &nn) {
-//     auto nameit = nameMap.find(name);
-//     if (nameit == nameMap.end()) {
-//         nameMap[name] = nn++;
-//         return nn - 1;
-//     }
-//     return nameit->second;
-// }
-
-// int resolveValue(Value &d, map<Jvalue, int> &valueMap, int &vn) {
-//     Jvalue value = Jvalue::factory(d);
-//     auto valueit = valueMap.find(value);
-//     if (valueit == valueMap.end()) {
-//         valueMap[value] = vn++;
-//         return vn - 1;
-//     }
-//     return valueit->second;
-// }
-
-int type_of(Value &d) {
-    if (d.GetType() != 6) return d.GetType();
-    return d.IsInt() ? 6 : 7;
+template <class SuccinctTree>
+void Cjson<SuccinctTree>::resolveValue(Value &d, uint nameId) {
+    // no name
+    if (isMaxUint(nameId)) {
+        // no data
+        if (d.IsNull())
+            values.append<Jval>(Jval(Jval::TYPE_NULL));
+        else if (d.IsObject())
+            values.append<Jval>(Jval(Jval::TYPE_OBJECT));
+        else if (d.IsArray())
+            values.append<Jval>(Jval(Jval::TYPE_ARRAY));
+        else if (d.IsFalse())
+            values.append<Jval>(Jval(Jval::TYPE_FALSE));
+        else if (d.IsTrue())
+            values.append<Jval>(Jval(Jval::TYPE_TRUE));
+        // with data (string)
+        else if (d.IsString()) {
+            uint strId = stringValues.size();
+            stringValues.push_back(string_to_cstr(d.GetString()));
+            values.append<StringJval>(StringJval(strId));
+        }
+        // with data (double)
+        else if (d.IsDouble()) {
+            values.append<DoubleJval>(DoubleJval(d.GetDouble()));
+        }
+        // with data (integer)
+        else if (d.IsNumber()) { // IsNumber returns true for doubles as well
+            int num = d.GetInt64();
+            if (intInChar(num))
+                values.append<CharJval>(CharJval(num));
+            else if (intInShort(num))
+                values.append<ShortJval>(ShortJval(num));
+            else
+                values.append<IntJval>(IntJval(num));
+        }
+        else { // assert we don't fall into an unknown type
+            assert(false);
+        }
+    }
+    // with name
+    else {
+        // no data
+        if (d.IsNull())
+            values.append<NamedJval>(NamedJval(Jval::TYPE_NULL, nameId));
+        else if (d.IsObject())
+            values.append<NamedJval>(NamedJval(Jval::TYPE_OBJECT, nameId));
+        else if (d.IsArray())
+            values.append<NamedJval>(NamedJval(Jval::TYPE_ARRAY, nameId));
+        else if (d.IsFalse())
+            values.append<NamedJval>(NamedJval(Jval::TYPE_FALSE, nameId));
+        else if (d.IsTrue())
+            values.append<NamedJval>(NamedJval(Jval::TYPE_TRUE, nameId));
+        // with data (string)
+        else if (d.IsString()) {
+            uint strId = stringValues.size();
+            stringValues.push_back(string_to_cstr(d.GetString()));
+            values.append<NamedStringJval>(NamedStringJval(nameId, strId));
+        }
+        // with data (double)
+        else if (d.IsDouble()) {
+            values.append<NamedDoubleJval>(NamedDoubleJval(nameId,
+                    d.GetDouble()));
+        }
+        // with data (integer)
+        else if (d.IsNumber()) { // IsNumber returns true for doubles as well
+            int num = d.GetInt64();
+            if (intInChar(num))
+                values.append<NamedCharJval>(NamedCharJval(nameId, num));
+            else if (intInShort(num))
+                values.append<NamedShortJval>(NamedShortJval(nameId, num));
+            else
+                values.append<NamedIntJval>(NamedIntJval(nameId, num));
+        }
+        else { // assert we don't fall into an unknown type
+            assert(false);
+        }
+    }
 }
 
 template <typename T> T* mapToArray(map<T, int> &mmap) {
@@ -99,6 +147,11 @@ template <typename T> T* mapToArray(map<T, int> &mmap) {
     for (auto it = mmap.begin(); it != mmap.end(); ++it)
         a[it->second] = it->first;
     return a;
+}
+
+template <class SuccinctTree>
+const Jval& Cjson<SuccinctTree>::operator[](const uint index) const {
+    return values.get<Jval>(index);
 }
 
 template <class SuccinctTree>
@@ -114,9 +167,16 @@ bool Cjson<SuccinctTree>::operator==(const Cjson &rhs) const {
 
 template <class SuccinctTree>
 ostream& operator<<(ostream &o, const Cjson<SuccinctTree> &p) {
-    o<<"Tree "<<p.tree<<endl;
-    o<<"Names "; for (int i = 0; i < p.names.size(); i++) o<<i<<":'"<<p.names[i]<<"'"; o<<endl;
-    return o<<"Values "<<p.values<<endl;
+    o<<"Tree: "<<p.tree<<endl;
+    o<<"Names ";
+    for (int i = 0; i < p.names.size(); i++)
+        o<<i<<":'"<<p.names[i]<<"'"; o<<endl;
+    o<<"Values ";
+    for (int i = 0; i < p.values.size(); i++) {
+        const Jval &v = p[i];
+        o<<":"<<v; if (v.isString()) o<<p.stringValues[v.getStringIndex()];
+    }
+    return o<<endl;
 }
 
 
--- src/cjson.hpp
+++ src/cjson.hpp
@@ -3,8 +3,10 @@
 #define __CJSON_HPP__
 
 #include "util.hpp"
-#include "jvalue.hpp"
+#include "jval.hpp"
 #include "bitmap_index.hpp"
+#include "bp_tree.hpp"
+#include "df_tree.hpp"
 #include "rapidjson/document.h"
 #include <iostream>
 #include <unordered_map>
@@ -20,23 +22,31 @@ public:
     SuccinctTree tree;
     vector<char*> names;
     BitmapIndex values;
+    vector<char*> stringValues;
 
 public:
     /* Constructors and destructor */
-    Cjson(Value &d, bool debug=false);
-    Cjson(int s, SuccinctTree &st, vector<char*> &nm, BitmapIndex &vl): size(s), tree(st), names(nm), values(std::move(vl)) {}
-
-    /* Methods */
-    void loadInfo(Value &d, unordered_map<string, int> &nt);
-    uint resolveNameId(const string &n, unordered_map<string, int> &nt);
+    Cjson(Value &d);
+    Cjson(int s, SuccinctTree &st, vector<char*> &nm, BitmapIndex &vl,
+            vector<char*> &sv) : size(s), tree(st), names(nm),
+                                 values(std::move(vl)), stringValues(sv) {}
+    // TODO: free all names and string values on destructor
 
     /* Operators */
+    const Jval& operator[](const uint index) const;
     bool operator==(const Cjson &rhs) const;
+
+private:
+    /* Private helper methods */
+    void loadInfo(Value &d, unordered_map<string, int> &nt, uint nId=MAX_UINT);
+    uint resolveNameId(const string &n, unordered_map<string, int> &nt);
+    void resolveValue(Value &d, uint nameId=MAX_UINT);
 };
 
-int resolveName(const string&, map<string, int>&, int&);
-int resolveValue(Value&, map<Jvalue, int>&, int&);
-int type_of(Value &d);
 template <typename T> T* mapToArray(map<T, int> &mmap);
+
+/* Typedefs and template instantiations */
+typedef Cjson<BpTree> CjsonBp;
+typedef Cjson<DfTree> CjsonDf;
 
 #endif
